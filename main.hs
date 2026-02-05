badddfed99d4fcb532d30a47d7fe5715872c3108
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-05 21:39:21 +0100

    feat: add sphere

--- main.hs
+++ main.hs
@@ -13,15 +13,27 @@ writePpmHeader handle width height = do
     hPutStrLn handle "255"
 
 
+hitSphere :: Point3 -> Double -> Ray -> Bool
+hitSphere center radius ray =
+    let oc = center `vecSub` rayOrigin ray
+        a  = dot (rayDirection ray) (rayDirection ray)
+        b  = -2.0 * dot (rayDirection ray) oc
+        c  = dot oc oc - radius * radius
+        discriminant = b * b - 4 * a * c
+    in discriminant >= 0
+
 rayColor :: Ray -> Vec3
 rayColor ray =
-    let unitDirection = unitVector (rayDirection ray)
-        a = 0.5 * (vecY unitDirection + 1.0)
-        white = Vec3 1.0 1.0 1.0
-        blue  = Vec3 0.5 0.7 1.0
-    in vecAdd (vecScale (1.0 - a) white) (vecScale a blue)
+    if hitSphere (Vec3 0 0 (-1)) 0.5 ray
+    then Vec3 1 0 0
+    else
+        let unitDirection = unitVector (rayDirection ray)
+            a = 0.5 * (vecY unitDirection + 1.0)
+            white = Vec3 1.0 1.0 1.0
+            blue  = Vec3 0.5 0.7 1.0
+        in vecAdd (vecScale (1.0 - a) white) (vecScale a blue)
 
-main :: IO ()
+
 main = do
     let aspectRatio = 16.0 / 9.0 :: Double
         imageWidth  = 400 :: Int
