8a7b2e3277c03b70a7b4b37e189cf20be8ec62d6
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-05 01:52:17 +0100

    chore: add progress indicator

--- main.hs
+++ main.hs
@@ -8,16 +8,20 @@ main = do
     hPutStrLn fileHandle "P3"
     hPutStrLn fileHandle (show imageWidth ++ " " ++ show imageHeight)
     hPutStrLn fileHandle "255"
-    sequence_
-        [ let red   = fromIntegral col / fromIntegral (imageWidth - 1)
-              green = fromIntegral (imageHeight - 1 - row) / fromIntegral (imageHeight - 1)
-              blue  = 0.25 :: Double
-              scaledRed   = floor (255.999 * red)   :: Int
-              scaledGreen = floor (255.999 * green) :: Int
-              scaledBlue  = floor (255.999 * blue)  :: Int
-           in hPutStrLn fileHandle (show scaledRed ++ " " ++ show scaledGreen ++ " " ++ show scaledBlue)
-        | row <- [0 .. imageHeight - 1]
-        , col <- [0 .. imageWidth - 1]
-        ]
+
+    mapM_ (\row -> do
+        hPutStr stderr ("\rScanlines remaining: " ++ show (imageHeight - row) ++ " ")
+        hFlush stderr
+        mapM_ (\col ->
+            let red   = fromIntegral col / fromIntegral (imageWidth - 1)
+                green = fromIntegral (imageHeight - 1 - row) / fromIntegral (imageHeight - 1)
+                blue  = 0.25 :: Double
+                scaledRed   = floor (255.999 * red)   :: Int
+                scaledGreen = floor (255.999 * green) :: Int
+                scaledBlue  = floor (255.999 * blue)  :: Int
+            in hPutStrLn fileHandle (show scaledRed ++ " " ++ show scaledGreen ++ " " ++ show scaledBlue)
+            ) [0 .. imageWidth - 1]
+        ) [0 .. imageHeight - 1]
+
+    hPutStrLn stderr "\rDone.                    "
     hClose fileHandle
-    hPutStrLn stderr "Done."
