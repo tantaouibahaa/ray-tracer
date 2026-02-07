dc01f8626fb051e4380eb0296649572c4e19ad48
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-07 01:30:18 +0100

    feat: intensity color

--- src/Color.hs
+++ src/Color.hs
@@ -2,10 +2,18 @@ module Color where
 
 import System.IO
 import Vec3
+import Interval
+
+linearToGamma :: Double -> Double
+linearToGamma x = if x > 0 then sqrt x else 0
 
 writeColor :: Handle -> Vec3 -> IO ()
 writeColor handle pixelColor =
-    let scaledRed   = floor (255.999 * vecX pixelColor) :: Int
-        scaledGreen = floor (255.999 * vecY pixelColor) :: Int
-        scaledBlue  = floor (255.999 * vecZ pixelColor) :: Int
-    in hPutStrLn handle (show scaledRed ++ " " ++ show scaledGreen ++ " " ++ show scaledBlue)
+    let intensity = Interval 0.000 0.999
+        r = clamp intensity (linearToGamma (vecX pixelColor))
+        g = clamp intensity (linearToGamma (vecY pixelColor))
+        b = clamp intensity (linearToGamma (vecZ pixelColor))
+        ir = floor (256 * r) :: Int
+        ig = floor (256 * g) :: Int
+        ib = floor (256 * b) :: Int
+    in hPutStrLn handle (show ir ++ " " ++ show ig ++ " " ++ show ib)
