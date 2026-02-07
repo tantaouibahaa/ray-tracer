04cd97da53e2e6fea2af223c88b5ef109d639521
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-07 01:30:56 +0100

    feat: import modules & add finalScene

--- main.hs
+++ main.hs
@@ -1,81 +1,74 @@
 module Main where
 
 import System.IO
-import Color
+import System.Random (mkStdGen)
 import Vec3
-import Ray
+import Hittable
+import Sphere
+import Camera
+import Random
 
+finalScene :: RNG -> ([Sphere], RNG)
+finalScene g0 =
+    let groundMat = Lambertian (Vec3 0.5 0.5 0.5)
+        ground = Sphere (Vec3 0 (-1000) 0) 1000 groundMat
 
-writePpmHeader :: Handle -> Int -> Int -> IO ()
-writePpmHeader handle width height = do
-    hPutStrLn handle "P3"
-    hPutStrLn handle (show width ++ " " ++ show height)
-    hPutStrLn handle "255"
+        big1 = Sphere (Vec3 0 1 0) 1.0 (Dielectric 1.5)
+        big2 = Sphere (Vec3 (-4) 1 0) 1.0 (Lambertian (Vec3 0.4 0.2 0.1))
+        big3 = Sphere (Vec3 4 1 0) 1.0 (Metal (Vec3 0.7 0.6 0.5) 0.0)
 
+        (smallSpheres, gFinal) = generateSmall (-11) (-11) g0 []
 
-hitSphere :: Point3 -> Double -> Ray -> Double
-hitSphere center radius ray =
-    let oc = center `vecSub` rayOrigin ray
-        a  = vecLengthSquared (rayDirection ray)
-        h  = dot (rayDirection ray) oc
-        c  = vecLengthSquard oc - radius * radius
-        discriminant = h * h -  a * c
-    in if discriminant < 0
-          then -1.0
-          else (-h - sqrt discriminant) / a
+    in (ground : big1 : big2 : big3 : smallSpheres, gFinal)
 
-rayColor :: Ray -> Vec3
-rayColor ray =
-    let t = hitSphere (Vec3 0 0 (-1)) 0.5 ray
-    in if t > 0.0
-       then
-           let normal = unitVector (rayAt ray t `vecSub` Vec3 0 0 (-1))
-           in vecScale 0.5 (Vec3 (vecX normal + 1) (vecY normal + 1) (vecZ normal + 1))
-       else
-           let unitDirection = unitVector (rayDirection ray)
-               a = 0.5 * (vecY unitDirection + 1.0)
-               white = Vec3 1.0 1.0 1.0
-               blue  = Vec3 0.5 0.7 1.0
-           in vecAdd (vecScale (1.0 - a) white) (vecScale a blue)
+generateSmall :: Int -> Int -> RNG -> [Sphere] -> ([Sphere], RNG)
+generateSmall a b g acc
+    | a > 10 = (acc, g)
+    | b > 10 = generateSmall (a + 1) (-11) g acc
+    | otherwise =
+        let (chooseMat, g1) = randomDouble g
+            (offX, g2) = randomDouble g1
+            (offZ, g3) = randomDouble g2
+            center = Vec3 (fromIntegral a + 0.9 * offX) 0.2 (fromIntegral b + 0.9 * offZ)
+            dist = vecLength (center `vecSub` Vec3 4 0.2 0)
+        in if dist <= 0.9
+           then generateSmall a (b + 1) g3 acc
+           else
+               let (sphere, g4) = makeSphere chooseMat center g3
+               in generateSmall a (b + 1) g4 (sphere : acc)
+
+makeSphere :: Double -> Point3 -> RNG -> (Sphere, RNG)
+makeSphere chooseMat center g
+    | chooseMat < 0.8 =
+        let (c1, g1) = randomVec3 g
+            (c2, g2) = randomVec3 g1
+            albedo = c1 `vecMul` c2
+        in (Sphere center 0.2 (Lambertian albedo), g2)
+    | chooseMat < 0.95 =
+        let (albedo, g1) = randomVec3Range 0.5 1.0 g
+            (fuzz, g2) = randomDoubleRange 0.0 0.5 g1
+        in (Sphere center 0.2 (Metal albedo fuzz), g2)
+    | otherwise =
+        (Sphere center 0.2 (Dielectric 1.5), g)
+
+main :: IO ()
 main = do
-    let aspectRatio = 16.0 / 9.0 :: Double
-        imageWidth  = 400 :: Int
-        imageHeight = max 1 (floor (fromIntegral imageWidth / aspectRatio)) :: Int
-
-        focalLength    = 1.0
-        viewportHeight = 2.0
-        viewportWidth  = viewportHeight * (fromIntegral imageWidth / fromIntegral imageHeight)
-        cameraCenter   = Vec3 0 0 0
-
-        viewportU = Vec3 viewportWidth 0 0
-        viewportV = Vec3 0 (-viewportHeight) 0
-
-        pixelDeltaU = vecDiv viewportU (fromIntegral imageWidth)
-        pixelDeltaV = vecDiv viewportV (fromIntegral imageHeight)
-
-        viewportUpperLeft = cameraCenter
-            `vecSub` Vec3 0 0 focalLength
-            `vecSub` vecDiv viewportU 2
-            `vecSub` vecDiv viewportV 2
-        pixel00Loc = viewportUpperLeft
-            `vecAdd` vecScale 0.5 (pixelDeltaU `vecAdd` pixelDeltaV)
+    let g0 = mkStdGen 42
+        (world, g1) = finalScene g0
+        cfg = defaultConfig
+            { cfgAspectRatio  = 16.0 / 9.0
+            , cfgImageWidth   = 400
+            , cfgSamplesPerPixel = 100
+            , cfgMaxDepth     = 50
+            , cfgVfov         = 20
+            , cfgLookFrom     = Vec3 13 2 3
+            , cfgLookAt       = Vec3 0 0 0
+            , cfgVUp          = Vec3 0 1 0
+            , cfgDefocusAngle = 0.6
+            , cfgFocusDist    = 10.0
+            }
+        cam = initCamera cfg
 
     fileHandle <- openFile "image.ppm" WriteMode
-    writePpmHeader fileHandle imageWidth imageHeight
-
-    mapM_ (\row -> do
-        hPutStr stderr ("\rScanlines remaining: " ++ show (imageHeight - row) ++ " ")
-        hFlush stderr
-        mapM_ (\col ->
-            let pixelCenter = pixel00Loc
-                    `vecAdd` vecScale (fromIntegral col) pixelDeltaU
-                    `vecAdd` vecScale (fromIntegral row) pixelDeltaV
-                rayDir = pixelCenter `vecSub` cameraCenter
-                ray = Ray cameraCenter rayDir
-                pixelColor = rayColor ray
-            in writeColor fileHandle pixelColor
-            ) [0 .. imageWidth - 1]
-        ) [0 .. imageHeight - 1]
-
-    hPutStrLn stderr "\rDone.                    "
+    render cam world fileHandle g1
     hClose fileHandle
